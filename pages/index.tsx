58224087bcef545b3c2c734bbdd224b28e50e448
sangock <senseoki@gmail.com>
2022-02-14 22:16:52 +0900

    ADD: init, header icon

---
+++ pages/index.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import styled from "styled-components";
+import Header from "../components/Header";
+
+const Container = styled.div`
+  font-size: 21px;
+  color: gray;
+`;
+
+const index: React.FC = () => {
+  return (
+    <Container>
+      Hello Airbnb
+    </Container>
+  );
+};
+
+export default index;
