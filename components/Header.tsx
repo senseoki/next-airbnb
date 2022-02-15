f2ac62c774a2befd93c2386ea7168a16d9c00f0b
sangock <senseoki@gmail.com>
2022-02-15 22:05:58 +0900

    ADD: header 회원가입, 로그인

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import styled from "styled-components";
 import AirbnbLogoIcon from "../public/static/svg/logo/logo.svg";
 import AirbnbLogoTextIcon from "../public/static/svg/logo/logo_text.svg";
+import palette from "../styles/palette";
+import Link from "next/link";
 
 const Container = styled.div`
   position: sticky;
@@ -22,14 +24,50 @@ const Container = styled.div`
       margin-right: 6px;
     }
   }
+
+  /** 헤더 로그인 회원가입 버튼 */
+  .header-auth-buttons {
+    .header-sign-up-button {
+      height: 42px;
+      margin-right: 8px;
+      padding: 0 16px;
+      border: 0;
+      border-radius: 21px;
+      background-color: white;
+      cursor: pointer;
+      outline: none;
+      &:hover {
+        background-color: ${palette.gray_f7};
+      }
+    }
+    .header-login-button {
+      height: 42px;
+      padding: 0 16px;
+      border: 0;
+      box-shadow: 0px 1px 2px rgba(0, 0, 0, 0.18);
+      border-radius: 21px;
+      background-color: white;
+      curson: pointer;
+      outline: none;
+      &:hover {
+        box-shadow: 0px 2px 8px rgba(0, 0, 0, 0, 0.12);
+      }
+    }
+  }
 `;
 
 const Header: React.FC = () => {
   return (
     <Container>
-      <div className="header-logo-wrapper">
-        <AirbnbLogoIcon className="header-logo" />
-        <AirbnbLogoTextIcon />
+      <Link href="/">
+        <a className="header-logo-wrapper">
+          <AirbnbLogoIcon className="header-logo" />
+          <AirbnbLogoTextIcon />
+        </a>
+      </Link>
+      <div className="header-auth-buttons">
+        <button type="button" className="header-sign-up-button">회원가입</button>
+        <button type="button" className="header-login-button">로그인</button>
       </div>
     </Container>
   )
